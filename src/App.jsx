7ea13e7e6c89f594a5e46464e070ce70b44e4a9b
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-11-04 17:08:39 +0700

    set darkmode and lightmode into localstorage with useColorScheme in MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import { useColorScheme } from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>tran quang thong</div>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
