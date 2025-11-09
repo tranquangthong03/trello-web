f84bc94b56fb847ce77c58ce2affe1b79ae990c7
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-11-09 19:33:49 +0700

    feat(appbar): create base structure with buttons and avatar
    - Added container layout for AppBar
    - Included placeholder buttons and basic avatar
    - Set up minimal styles and alignment

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -1,7 +1,35 @@
-function index() {
+import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined'
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
-    <div>index</div>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light"><Box sx={{ display:'flex', alignItems:'center', gap:1 }}><LightModeIcon /> Light</Box></MenuItem>
+        <MenuItem value="dark"><Box sx={{ display:'flex', alignItems:'center', gap:1 }}><DarkModeOutlinedIcon /> Dark</Box></MenuItem>
+        <MenuItem value="system"><Box sx={{ display:'flex', alignItems:'center', gap:1 }}><SettingsBrightnessOutlinedIcon /> System</Box></MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
-export default index
+export default ModeSelect
