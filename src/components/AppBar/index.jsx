5cf3ac63ddc71764f9282a6faa986ba1ab700520
tranquangthong <23115053122141@sv.ute.udn.vn>
2025-11-12 00:18:04 +0700

    feat: add BoardBar component

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -15,14 +15,17 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import Tooltip from '@mui/material/Tooltip'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import Profile from './Menus/Profile'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
+
 function AppBar() {
   return (
-    <Box px={2} sx={{
+    <Box sx={{
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
+      px: 2,
       gap: 2,
       overflowX: 'auto'
     }}>
@@ -38,7 +41,7 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={<LibraryAddIcon />}>Create</Button>
         </Box>
       </Box>
       <Box sx={{ display:'flex', alignItems:'center', gap:2 }}>
